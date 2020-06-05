2fa53e21c562d1c3398c54223e0fc5595beaf916
jtopgi <jtopgi@users.noreply.github.com>
2020-06-04 23:15:26 -0500

    clean up searches workflow.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,12 +1,10 @@
 class SearchesController < ApplicationController
-  def new
-  end
+  def new;end
 
   def show
-    @trails = nil
-    return unless params[:q]
+    return unless params[:location]
 
-    coordinates = Geocoder.search(params[:q]).first.coordinates
+    coordinates = Geocoder.search(params[:location]).first.coordinates
     @trails = HikingProjectService.get_trails(coordinates)
   end
 end
