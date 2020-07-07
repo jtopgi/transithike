5a8ee0c7e96049e15dfe5d43bb8f7b41de5f51ab
jtopgi <jtopgi@users.noreply.github.com>
2020-07-06 22:22:15 -0500

    fix js, css, and update rails.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,8 +2,9 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
 require("@rails/ujs").start()
-require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
@@ -14,7 +15,8 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-import "./styles.scss"
+
+import "./application.scss"
 require("bootstrap")
 // Enable popovers globally
 $(function () {
@@ -22,3 +24,4 @@ $(function () {
 })
 
 function requireAll(r) { r.keys().forEach(r); }
+console.log("hello");
