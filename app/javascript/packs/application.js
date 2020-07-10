2fd350f32443044e38e6102a547070224ca8b8bd
Jeevan Topgi <jtopgi@users.noreply.github.com>
2020-07-09 21:04:35 -0500

    Bugfix/fix css loading. (#8)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,7 +16,7 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-import "./application.scss"
+import "./application.css"
 require("bootstrap")
 // Enable popovers globally
 $(function () {
