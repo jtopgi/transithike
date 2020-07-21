91d56e3f91381a701e7721a4ab66abf8c2b99d33
jtopgi <jtopgi@users.noreply.github.com>
2020-07-20 22:49:10 -0500

    add connection to google street view.

--- app/services/trails_service.rb
+++ app/services/trails_service.rb
@@ -6,6 +6,13 @@ module TrailsService
     "travelmode=transit"
   ].join('&')
 
+  GOOGLE_STREET_IMAGE = [
+    "https://maps.googleapis.com/maps/api/streetview?parameters",
+    "location=%{location}",
+    "size=500x500",
+    "key=#{Rails.application.credentials.google_maps_key}"
+  ].join('&')
+
   def self.get_trails(origin:, arrival_time:, maximum_length:)
     trails = HikingProjectService.get_trails(
       lat: origin.latitude, lon: origin.longitude, maximum_length: maximum_length
@@ -18,6 +25,9 @@ module TrailsService
         trail.duration = Google::Maps.route(origin, trailhead, options).duration
         direction = {origin: origin, destination: trailhead}
         trail.google_maps_url = GOOGLE_MAPS_URL % direction
+        next if trail.imgMedium.present?
+
+        trail.imgMedium = GOOGLE_STREET_IMAGE % {location: trailhead}
       rescue
         next
       end
